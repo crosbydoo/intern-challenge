2e53cbcfa679f19738c5b0fe83c0b59abd034cf0
Crosbydev <xxristuaji141@gmail.com>
2023-12-10 13:18:58 +0700

    fixing usecase

--- lib/core/usecase/use_case.dart
+++ lib/core/usecase/use_case.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: one_member_abstracts
+
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ristu_intern_challenge/core/shared/error/failures.dart';
